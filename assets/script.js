5e6c414a222e0aa51d92ae2a0f1a9bc3d82da1f8
Josh Rivera <beps@Mactop.local>
2023-12-30 11:42:20 -0600

    Added logic for gameplay, still working out kinks

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,51 @@
 document.addEventListener('DOMContentLoaded',async () => {
     
+    //Stick logic
+    
+    //Initial amount of sticks for each player
+    let initialSticks = 1;
+    //Maximum amount of sticks per bundle
+    let maxSticks = 4;
+    
+    //Object containing bundle values
+    let stickBundle = {
+        p1Bundle: initialSticks,
+        p2Bundle: initialSticks,
+        p1Bundle2: initialSticks,
+        p2Bundle2: initialSticks
+    }
+    
+
+    //Function for showing amount of sticks
+    function handleBundles() {
+        for (let bundle in stickBundle) {
+            document.getElementById(bundle).innerText = bundle + " " + stickBundle[bundle]
+        }
+    }
+    
+    //Function for adding sticks between bundles
+    function handleSticks(bundle, target) {
+        //Adding current bundle value to the target bundle
+        stickBundle[target] += stickBundle[bundle]
+
+        //Disable bundle if bundle exceeds max amount of sticks
+        if(stickBundle[bundle] > maxSticks) {
+            document.getElementById(bundle).disabled = true;
+        }
+
+        //Run handleBundles to update bundles
+        handleBundles()
+    }
+
+    //Event listeners for bundles
+    document.getElementById('p1Bundle').addEventListener('click', () => {
+        handleSticks('p1Bundle', 'p2bundle');
+    })
+    
+    document.getElementById('p1Bundle2').addEventListener('click', () => {
+        handleSticks('p1Bundle2', 'p2Bundle2')
+    })
+    handleBundles()
 
     //Button Logic
     let footer = document.getElementById('pageFoot')
@@ -11,7 +57,7 @@ document.addEventListener('DOMContentLoaded',async () => {
             footer.removeChild(infoDiv)
         } else {
             infoDiv = document.createElement('div')
-            infoDiv.textContent = "Sticks is a turn based player vs player game that is very simple. Each player is given two bundles, and each bundle can hold up to 4 sticks. Players will take turns adding their amount of sticks to the other player(without losing their own), until the player reaches 4 sticks. If a bundle goes over 4 sticks, their bundle will break and cannot be used for giving/taking. The game is won if you break both of the opponents bundles. If a player loses one of their bundles, that bundle is destroyed until the player makes a split. A split is a move that will divide your current sticks into 2 equal bundles and reinstate your broken bundle. However, a split can only be performed on bundles that have an even number of sticks(E.g. A player can split a bundle of 2 or 4, but not 1 or 3). "
+            infoDiv.textContent = "Game Info"
             infoDiv.setAttribute('class', 'container-xl flex-grow-1')
             infoDiv.setAttribute('id', 'gameInfo')
             footer.append(infoDiv)
